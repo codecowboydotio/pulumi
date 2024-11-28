b256707ae5dd4273533bbe52ac14bc4f9ee704cd
codecowboydotio <scottvankalken@gmail.com>
2024-11-28 22:56:40 +1100

    stack config

--- do-app/__main__.py
+++ do-app/__main__.py
@@ -1,16 +1,25 @@
 import pulumi
 import pulumi_digitalocean as digitalocean
 
+stack_config = pulumi.Config("cfg")
+var_app_name = stack_config.require("app-name")
+var_app_size = stack_config.require("app-size")
+var_service_name = stack_config.require("service-name")
+var_branch = stack_config.require("branch")
+
 python_sample = digitalocean.App("python-sample", spec={
-    "name": "python-sample",
+    "name": var_app_name,
     "region": "ams",
     "services": [{
-        "name": "py-service",
-        "instance_count": 1,
-        "instance_size_slug": "apps-s-1vcpu-1gb",
+        "name": var_service_name,
+        "instance_count": 2,
+        "instance_size_slug": var_app_size,
         "git": {
             "repo_clone_url": "https://github.com/codecowboydotio/do-pulumi-app",
-            "branch": "main",
+            "branch": var_branch,
         },
     }],
 })
+
+pulumi.export('app_url', python_sample.live_url)
+
