162c57c78a67f17b53042b11bf94964cfbf32950
codecowboydotio <scottvankalken@gmail.com>
2025-03-01 10:51:58 +1100

    test

---
+++ aws-serverless/iam.py
@@ -0,0 +1,39 @@
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
+from pulumi_aws import iam
+
+lambda_role = iam.Role(
+    "lambdaRole",
+    name="svk-lambda-role",
+    assume_role_policy="""{
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Action": "sts:AssumeRole",
+                "Principal": {
+                    "Service": "lambda.amazonaws.com"
+                },
+                "Effect": "Allow",
+                "Sid": ""
+            }
+        ]
+    }""",
+)
+
+lambda_role_policy = iam.RolePolicy(
+    "lambdaRolePolicy",
+    name="svk-lambda-role-policy",
+    role=lambda_role.id,
+    policy="""{
+        "Version": "2012-10-17",
+        "Statement": [{
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "arn:aws:logs:*:*:*"
+        }]
+    }""",
+)
